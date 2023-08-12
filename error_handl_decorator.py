cb50c40f1f109108a9955b0dd8ab6a02fca57959
MicMaus <michael.yasynskyi@gmail.com>
2023-08-12 18:58:35 +0200

    v2

--- error_handl_decorator.py
+++ error_handl_decorator.py
@@ -4,11 +4,11 @@ def error_handling_decorator(func):
             return func(*args, **kwargs)
         except (ValueError, KeyError, IndexError) as error:
             error_message = {
-                ValueError: "please provide name and phone number divided by space",
-                KeyError: "please provide valid name",
-                IndexError: "phone book is empty", 
+                ValueError: "please provide valid value",
+                KeyError: "please provide valid input",
+                IndexError: "no records found", 
             }.get(type(error))
             
-            print(f"{error_message}")
+            return(f"{error_message}")
     
     return inner
